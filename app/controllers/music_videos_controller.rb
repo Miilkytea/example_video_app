b662446bb2f8a71a4a847763ae0ea796f7f882b7
h4w5 <philip@h4w5.com>
2015-07-24 16:29:02 -0700

    working: ... band rep can add or remove videos / auth
    adds authenticate/ authorize before actions to music video flow

--- app/controllers/music_videos_controller.rb
+++ app/controllers/music_videos_controller.rb
@@ -1,15 +1,14 @@
 class MusicVideosController < ApplicationController
+  before_action :authenticate # do it for all!
+  before_action :authorize    # do it for all!
+
   def new
-    # since we are nested, we can just get the band id
-    # from the params
-    @band = Band.find(params[:id])
+    # @band was created in the authorize method!
     @music_video = MusicVideo.new
   end
 
   def create
-    # since we are nested, we can just get the band id
-    # from the params
-    @band = Band.find(params[:id])
+    # @band was created in the authorize method!
     music_video = MusicVideo.new(music_video_params)
     @band.music_videos << music_video
 
@@ -21,6 +20,26 @@ class MusicVideosController < ApplicationController
   end
 
 private
+
+  def authenticate
+    unless logged_in?
+      flash.now.alert = 
+        "You must be logged in to access this section of the site."
+      redirect_to login_path
+    end
+  end
+
+  def authorize
+    # since we are nested, we can just get the band id
+    # from the params
+    @band = Band.find(params[:band_id])
+
+    unless current_user.band == @band
+      flash.now.alert = 
+        "You are not authorized to edit this band's information."
+      redirect_to band_path(@band)
+    end
+  end
 
   def music_video_params
     params.require(:music_video).permit(
