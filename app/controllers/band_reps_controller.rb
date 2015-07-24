f5ba3ac191c0b894f7f8e434f5b4ee9bdbfe19a1
h4w5 <philip@h4w5.com>
2015-07-24 14:35:53 -0700

    working: ... log in as a representative of a band / sign up
    adds the sign up as a new user flow (new, create)
    adds new user (band rep) form
    adds flash messages to layout

---
+++ app/controllers/band_reps_controller.rb
@@ -0,0 +1,26 @@
+class BandRepsController < ApplicationController
+  def new
+    @band_rep = BandRep.new
+  end
+
+  def create
+    @band_rep = BandRep.new(band_rep_params)
+    if @band_rep.save
+      # notice adds a welcome message to the *flash*!
+      redirect_to root_path, notice: "Welcome new user."
+    else
+      render :new
+    end
+  end
+
+private
+
+  def band_rep_params
+    params.require(:band_rep).permit(
+      :username, 
+      :password, 
+      :password_confirmation, 
+      :band_id
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "bands#index"
 
   resources :bands, only: [:index, :show]
+  resources :band_reps, only: [:new, :create]
 end
