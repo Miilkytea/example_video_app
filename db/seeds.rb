93274e59ebcbc51c958cc2f2a137ab6efb46c4b2
h4w5 <philip@h4w5.com>
2015-07-24 09:53:27 -0700

    create initial seed file to set up test db data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Band.destroy_all
+MusicVideo.destroy_all
+
+mac = Band.create(
+  name:  "Fleetwood Mac", 
+  genre: "Country Rock"
+)
+ham = Band.create(
+  name:  "Haim",
+  genre: "Country Rock Revival Pop"
+)
+
+mac.music_videos << MusicVideo.create(
+  title:           "Landslide", 
+  video_embed_uri: "ZqCotgdEZsk"
+)
+mac.music_videos << MusicVideo.create(
+  title:           "Blue Letter", 
+  video_embed_uri: "7DKlOD9Qs_k"
+)
+mac.music_videos << MusicVideo.create(
+  title:           "Never Going Back Again", 
+  video_embed_uri: "GGyCx9WgGV8"
+)
+
+ham.music_videos << MusicVideo.create(
+  title:           "Save Me", 
+  video_embed_uri: "kiqIush2nTA"
+)
+ham.music_videos << MusicVideo.create(
+  title:           "Forever", 
+  video_embed_uri: "sEwM6ERq0gc"
+)
